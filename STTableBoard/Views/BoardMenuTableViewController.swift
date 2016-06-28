c2646027cde5a20dcb49db3793da0947c537c522
StormXX <liaozhaoxing@gmail.com>
2016-06-28 16:42:54 +0800

    fix boardmenu crash on iOS 8

--- STTableBoard/Views/BoardMenuTableViewController.swift
+++ STTableBoard/Views/BoardMenuTableViewController.swift
@@ -15,6 +15,10 @@ class BoardMenuTableViewController: UITableViewController {
         super.init(style: style)
     }
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MenuCell")
